14f2284c9a320bc371622a6f9925a0a8f500d355
Ambrose <152869@cognizant.com>
2017-04-11 11:50:34 -0500

    Modified Main.ts

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,8 @@
+/*
+This is Main Page which will be mapped to Index.html using Main.js file which is configured in systemjs.config.js
+ */
+
+
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import{AppModule} from './app.module';
 
